2d1360809276b8026ad67848ce89d3cacf3957ae
rendix2 <rendix2@seznam.cz>
2015-07-17 14:54:17 +0200

    repaired 2

---
+++ QW/Templates/Web/Default/User/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tomáš
+ * Date: 17. 7. 2015
+ * Time: 14:20
+ */
+
+
+use QW\FW\Forms\FormCreator\FormCreatorHTML4;
+
+$z = new FormCreatorHTML4('POST', '');
+$z->createSelect('Select',)->addList()->addChoice();
+$z->addSelect()->addTextArea('text')->addInputText('ahoj', 'ahoj', 5, 50,'off');
--- index.php
+++ index.php
@@ -3,29 +3,8 @@
 include('./autoLoader.php');
 include('./debug.php');
 
-use QW\FW\Forms\FomCreator\FormCreator;
-use QW\FW\Singleton;
-
-class MySingleton extends Singleton
-{
-
-    public function r()
-    {
-        echo 'da';
-    }
-}
-
-//$z = MySingleton::getSingleton();
-
-//$z->r();
-
 
 $quickWeb = new \QW\Libs\Bootstrap();
-
-
-$z = new FormCreator('POST', '');
-$z->createSelect('Select')->addList()->addChoice();
-$z->addSelect();
 
 ?>
 

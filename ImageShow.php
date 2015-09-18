dcc592eeffa3392c7653152a5c2e4c562bb0abae
rendix2 <rendix2@seznam.cz>
2015-09-19 00:20:20 +0200

    Better ImageTextGenerator

--- ImageShow.php
+++ ImageShow.php
@@ -10,6 +10,10 @@ $img->setTextHorizontally( 7, 100, 29, 'FUCK YOU!' );
 $img->setTextHorizontally( 7, 100, 99, 'FUCK YOU!' );
 $img->setTextVertically( 7, 80, 109, 'FUCK YOU!' );
 $img->setTextVertically( 7, 180, 109, 'FUCK YOU!' );
+
+for ( $i = 110; $i < 160; $i += 5 ) $img->setCharHorizontally( 7, $i, 70, '!' );
+
+
 $img->toPNG();
 header( 'Content-Type: image/png' );
 

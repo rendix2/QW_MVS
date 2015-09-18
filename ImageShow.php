73a4ad96e0486394468c95b6137d57f662deef41
rendix2 <rendix2@seznam.cz>
2015-09-18 23:23:05 +0200

    New Image Colors

--- ImageShow.php
+++ ImageShow.php
@@ -3,9 +3,9 @@ include( './autoLoader.php' );
 //include( './debug.php' );
 
 
-$img = new QW\FW\Images\ImageTextGenerate( 200, 40 );
-$img->setBackgroundColor( 235, 205, 235 );
-$img->setTextColor( 235, 4, 3 );
+$img = new QW\FW\Images\ImageTextGenerate( 400, 80 );
+$img->setBackgroundColor( 255, 255, 255 );
+$img->setTextColor( 255, 15, 25 );
 $img->setText( 5, 0, 15, \QW\Libs\Config::EMAIL );
 $img->toPNG();
 header( 'Content-Type: image/png' );

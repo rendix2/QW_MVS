a5da3cf94f60fb70a77df6a2135c53c3d713bb42
rendix2 <rendix2@seznam.cz>
2015-10-16 00:07:43 +0200

    isPalindromeBetter

--- QW/FW/Basic/String.php
+++ QW/FW/Basic/String.php
@@ -8,7 +8,11 @@ use QW\FW\Validator;
 
 class String extends Object {
 	const MANACHER_DELIMITER = '|';
-	protected static $separators;
+	protected static $separators = [ "\n", "\r", "\r\n", "\n\r", chr( 30 ), chr( 155 ), PHP_EOL ]
+	protected static $diac = [ 'ľ', 'š', 'č', 'ť', 'ž', 'ý', 'á', 'í', 'é', 'Č', 'Á', 'Ž', 'Ý' ];
+	protected static $cor = [ 'l', 's', 'c', 't', 'z', 'y', 'a', 'i', 'e', 'C', 'A', 'Z', 'Y' ];
+
+
 	protected $string;
 	protected $matches;
 	protected $length;
@@ -20,7 +24,6 @@ class String extends Object {
 		$this->matches    = NULL;
 		$this->debug = $debug;
 		$this->length = mb_strlen( $this->string, 'UTF-8' );
-		self::$separators = [ "\n", "\r", "\r\n", "\n\r", chr( 30 ), chr( 155 ), PHP_EOL ];
 	}
 
 	function __destruct() {
@@ -105,8 +108,6 @@ class String extends Object {
 	public function concatPostString( String $string ) {
 		return new String( $string->string . $string->string, $this->debug );
 	}
-
-	// http://php.net/manual/en/function.nl2br.php
 
 	public function concatPre( $string ) {
 		return new String( (string) $string . $this->string, $this->debug );
@@ -285,10 +286,7 @@ class String extends Object {
 	}
 
 	public function removeDiacritics() {
-		static $diac = [ 'ľ', 'š', 'č', 'ť', 'ž', 'ý', 'á', 'í', 'é', 'Č', 'Á', 'Ž', 'Ý' ];
-		static $cor = [ 'l', 's', 'c', 't', 'z', 'y', 'a', 'i', 'e', 'C', 'A', 'Z', 'Y' ];
-
-		return $this->replace( $diac, $cor );
+		return $this->replace( self::$diac, self::$cor );
 	}
 
 	public function removeHTMLTags( $allowable_tags = NULL ) {
